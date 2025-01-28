e4b7d9604b0d4dfbaeb190b4ba575aa5cd993024
CVS CHARAN <cvs@CVSs-MacBook-Pro.local>
2025-01-29 01:55:48 +0530

    added email.js to contact

--- src/sections/NavBar.tsx
+++ src/sections/NavBar.tsx
@@ -1,15 +1,15 @@
 "use client";
 
-import { useState } from "react";
+// import { useState } from "react";
 import Link from "next/link";
 import styles from "../styles/NavBar.module.css";
 
 const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  // const [isOpen, setIsOpen] = useState(false);
 
-  const toggleMenu = () => {
-    setIsOpen(!isOpen);
-  };
+  // const toggleMenu = () => {
+  //   setIsOpen(!isOpen);
+  // };
 
   return (
     <nav id="DSN Events Nav" className={styles.navbar}>
